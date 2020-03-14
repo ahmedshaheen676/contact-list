f0e6529186e30144c5f275f2290ac671fd7f456d
ahmedshaheen <ahmedshaheen676@yahoo.com>
2020-03-14 16:49:12 +0200

    add model

---
+++ src/main/java/com/iti/model/Contact.java
@@ -0,0 +1,69 @@
+package com.iti.model;
+
+public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private Gender gender;
+
+    public Contact() {
+    }
+
+    public Contact(String firstName, String lastName, String phone, String email, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
---
+++ src/main/java/com/iti/model/Gender.java
@@ -0,0 +1,5 @@
+package com.iti.model;
+
+public enum Gender {
+    Male, Female;
+}
